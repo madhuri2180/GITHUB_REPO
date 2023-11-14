3627c065437fb9b5b8b4927f97bd3ec4253e145b
madhuri2180 <burrimadhuri123@gmil.com>
2023-11-14 20:10:29 +0530

    first commit

---
+++ src/main/java/GITPROMPT1/GITPROMPT1/App.java
@@ -0,0 +1,13 @@
+package GITPROMPT1.GITPROMPT1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
